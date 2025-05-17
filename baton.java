723e428780de31ce7330c1a75562a79edde2534c
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-05-17 15:46:46 +0200

    Dodano nowy plik: baton.java

---
+++ baton.java
@@ -0,0 +1,6 @@
+public class Baton {
+    public static void main(String[] args) {
+        // Powitanie użytkownika
+        System.out.println("Witaj w świecie kazachstańskich batonów!");
+    }
+}
